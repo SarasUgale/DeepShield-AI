47f952430c6de087f62567a7ab22d2a01923d4b6
Saras <sarasugale@gmail.com>
2026-06-09 18:06:58 +0530

    Initial commit

--- static/js/parallax.js
+++
@@ -1,4 +0,0 @@
-// lightweight parallax using Rellax CDN
-import('https://cdn.jsdelivr.net/npm/rellax@1.12.1/rellax.min.js').then(() => {
-  new Rellax('[data-rellax-speed]', { center: true });
-});
